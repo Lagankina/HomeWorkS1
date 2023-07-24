ffcceb3be973612f29472e73d91ed39190198509
Adel <reirianet@mail.ru>
2023-07-24 15:06:10 +0300

    Домашняя работа семинар3 ВСЕ

--- main.py
+++ main.py
@@ -162,19 +162,39 @@
 
 # ////////////////////////////////////////////
 
-list_1 = [2, 4, 1, 6, 8, 2, 9, 3, 2, 5]
-k = 1 #9
-min_result = [len(list_1)]
+# list_1 = [2, 4, 1, 6, 8, 2, 9, 3, 2, 5]
+# k = 1 #9
+# min_result = [len(list_1)]
 
-for element in list_1:
-    min_result.append(abs(k - element))
+# for element in list_1:
+#     min_result.append(abs(k - element))
 
-min = 999999999999
-min_index = 0
-index = 0
-for index in range(1, len(min_result)):
-    if (min > min_result[index]):
-        min = min_result[index]
-        min_index = index
+# min = 999999999999
+# min_index = 0
+# index = 0
+# for index in range(1, len(min_result)):
+#     if (min > min_result[index]):
+#         min = min_result[index]
+#         min_index = index
 
-print(list_1[min_index - 1])
+# print(list_1[min_index - 1])
+
+# //////////////////////////////////////////////////////
+
+# k = input("Введите слово: ").upper()
+# summ = 0
+# dict = {"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "S": 1, "T": 1, "R": 1, 
+#             "D": 2, "G": 2,"B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, 
+#             "V": 4, "W": 4, "Y": 4,"K": 5, "J": 8, "X": 8, "Q": 10, "Z": 10,
+#             "А": 1, "В": 1 , "Е": 1, "И": 1, "Н": 1, "О": 1, "Р": 1, "С": 1, "Т": 1, 
+#             "Д": 2, "К": 2, "Л": 2, "М": 2, "П": 2, "У": 2, "Б": 3, "Г": 3, "Ё": 3, "Ь": 3, "Я": 3, 
+#             "Й": 4, "Ы": 4, "Ж": 5, "З": 5, "Х": 5, "Ц": 5, "Ч": 5, "Ш": 8, "Э": 8, "Ю": 8, "Ф": 10, "Щ": 10, "Ъ": 10}
+# for i in k:
+#     summ += dict[i]
+# print(summ)
+
+
+
+
+
+
